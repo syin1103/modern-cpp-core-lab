3ad8358d4f58a73e600c6e6328f0c514bd6349dd
syin1103 <syin1103@gmail.com>
2026-03-09 17:49:05 +0800

    feat: ch13-inherit constructor
    Signed-off-by: syin1103 <syin1103@gmail.com>

---
+++ src/ch13_inherit_constructor/13-2-2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Base {
+ public:
+  Base() : x_(0), y_(0.) {};
+  Base(int x, double y) : x_(x), y_(y) {}
+  Base(int x) : x_(x), y_(0.) {}
+  Base(double y) : x_(0), y_(y) {}
+
+ private:
+  int x_;
+  double y_;
+};
+
+class Derived : public Base {
+ public:
+  using Base::Base;
+};
+
+int main() {
+  Base b;
+  Derived d;
+  return 0;
+}
